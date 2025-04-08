fbba5ffff4391630b6004d85de5a7d06d27ece25
neilchandarana <neilchanda@hotmail.com>
2025-04-08 13:25:14 +0100

    apply changes, update config

--- apps/api/src/links/links.controller.ts
+++ apps/api/src/links/links.controller.ts
@@ -1,43 +1,35 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
-import { LinksService } from './links.service';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common"
+import type { LinksService } from "./links.service"
 
-import { CreateLinkDto } from '@common/api/links/dto/create-link.dto';
-import { UpdateLinkDto } from '@common/api/links/dto/update-link.dto';
+import type { CreateLinkDto } from "@common/api/links/dto/create-link.dto"
+import type { UpdateLinkDto } from "@common/api/links/dto/update-link.dto"
 
-@Controller('links')
+@Controller("links")
 export class LinksController {
   constructor(private readonly linksService: LinksService) {}
 
   @Post()
   create(@Body() createLinkDto: CreateLinkDto) {
-    return this.linksService.create(createLinkDto);
+    return this.linksService.create(createLinkDto)
   }
 
   @Get()
   findAll() {
-    return this.linksService.findAll();
+    return this.linksService.findAll()
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.linksService.findOne(+id);
+  @Get(":id")
+  findOne(@Param("id") id: string) {
+    return this.linksService.findOne(+id)
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLinkDto: UpdateLinkDto) {
-    return this.linksService.update(+id, updateLinkDto);
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateLinkDto: UpdateLinkDto) {
+    return this.linksService.update(+id, updateLinkDto)
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.linksService.remove(+id);
+  @Delete(":id")
+  remove(@Param("id") id: string) {
+    return this.linksService.remove(+id)
   }
 }
--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,7 +1,7 @@
-import { Link } from 'links/entities/link.entity';
+import { Link } from "links/entities/link.entity"
 
-import { CreateLinkDto } from 'links/dto/create-link.dto';
-import { UpdateLinkDto } from 'links/dto/update-link.dto';
+import { CreateLinkDto } from "links/dto/create-link.dto"
+import { UpdateLinkDto } from "links/dto/update-link.dto"
 
 export const links = {
   dto: {
@@ -11,4 +11,4 @@ export const links = {
   entities: {
     Link,
   },
-};
+}
