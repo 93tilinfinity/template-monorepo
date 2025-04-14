cae9345a30b7cbcf45692e191d7181bc14bd5740
neilchandarana <neilchanda@hotmail.com>
2025-04-14 21:02:38 +0100

    linted

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,8 @@
 import "./instrument"
 
 import { NestFactory } from "@nestjs/core"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 import { Logger, LoggerErrorInterceptor } from "nestjs-pino"
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
 import { AppModule } from "./app.module"
 
 import fs from "node:fs"
